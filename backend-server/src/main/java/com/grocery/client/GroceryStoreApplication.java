3ad1031d6c6ab6020cba1a75a73f6269b02a98e1
MOHAMMADMODASSIR <66721527+MOHAMMADMODASSIR@users.noreply.github.com>
2025-08-01 21:21:15 +0530

    Merge Changes into Feature (#1)
    * Added ENV
    
    * Common Git File

--- backend-server/src/main/java/com/grocery/client/GroceryStoreApplication.java
+++ backend-server/src/main/java/com/grocery/client/GroceryStoreApplication.java
@@ -1,5 +1,6 @@
 package com.grocery.client;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -9,6 +10,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class GroceryStoreApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("APP_NAME", dotenv.get("APP_NAME"));
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(GroceryStoreApplication.class, args);
 	}
 
